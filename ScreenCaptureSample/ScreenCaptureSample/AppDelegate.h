aa69c6be2086aa35f5dab41280d2696ab51dbcb3
devtang <rsup@rsupui-MacBookPro.local>
2026-05-07 22:41:06 +0900

    feat:add new project

---
+++ ScreenCaptureSample/ScreenCaptureSample/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ScreenCaptureSample
+//
+//  Created by devtang on 5/7/26.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
